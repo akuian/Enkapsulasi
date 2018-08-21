6d375f7631c68b28b5d6eb9b6bd618277e0c8a4e
Hafiyyan Faza Pratama <42332037+akuian@users.noreply.github.com>
2018-08-21 14:05:21 +0700

    Update UjiBus.java

--- src/TugasPraktikum/UjiBus.java
+++ src/TugasPraktikum/UjiBus.java
@@ -10,5 +10,20 @@ package TugasPraktikum;
  * @author Hafiyyan Faza XIR5
  */
 public class UjiBus {
-    
+    public static void main(String[] args) {
+         Bus Uji = new Bus(12);
+        Uji.penumpang = 5;
+        Uji.maxPenumpang = 15;
+        Uji.cetak();
+        
+        Uji.penumpang=Uji.penumpang + 5;
+        Uji.cetak();
+        
+        Uji.penumpang = Uji.penumpang - 2;
+        Uji.cetak();
+        
+        Uji.penumpang = Uji.penumpang + 8;
+        Uji.cetak();
+        Uji.getAverage(0);
+    }
 }
